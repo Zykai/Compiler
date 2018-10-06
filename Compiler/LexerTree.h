7a2656866233d3fd4bf03d42c125f38025c74a5f
Tobias Palzer <tobiaspalzer.tp@gmail.com>
2018-10-06 15:02:01 +0200

    remove required

--- Compiler/LexerTree.h
+++ Compiler/LexerTree.h
@@ -1,16 +1,17 @@
+#pragma once
 #include <list>
 #include "Token.h"
 
 // TODO:
 //	-create root-tree class
 //  -create string class
-//	-create constant class (while etc.)
+//	-create number class
+//	-create constant class (while, +,  etc.)
 
 
 class LexerTree {
 public:
 	LexerTree * nextTrees[128];
-	char required;
 	virtual LexerTree* processChar(char & nextChar) = 0;
 private:
 

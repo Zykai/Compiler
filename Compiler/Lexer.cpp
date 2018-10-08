ab9b71a11b5dbe65d72f5d9cc27a9c8dc2061bed
Tobias Palzer <tobiaspalzer.tp@gmail.com>
2018-10-08 13:39:26 +0200

    add algorithm for one line
    lexical analysis for one line; needs to be tested

--- Compiler/Lexer.cpp
+++ Compiler/Lexer.cpp
@@ -1,20 +1,22 @@
 #include "stdafx.h"
 #include "Lexer.h"
 #include <iostream>
+#include <cctype>
 
 std::list<Token> Lexer::getTokenList() {
 	this->buildLexerTree();
-	std::cout << source->text;
-	const char * cstring = source->text.c_str();
+	source->print();
+	//const char * cstring = source->text.c_str();
 
-	while (false) {
-		Token currentToken = getToken();
+	for (std::string &s : this->source->textList) {
+		this->analyzeLine(s);
 	}
 	return std::list<Token>();
 }
 
 Lexer::Lexer(std::string source) {
 	this->source = new SourceFile(source);
+	this->noError = true;
 }
 
 
@@ -23,11 +25,33 @@ Lexer::~Lexer() {
 	delete root;
 }
 
-Token Lexer::getToken() {
-	return Token();
-}
-
 void Lexer::buildLexerTree() {
 	// TODO
 	//root = new LexerTree();
+}
+
+// TODO test algorith, needs root implementation first, fails currently
+void Lexer::analyzeLine(std::string & line) {
+	LexerTree * currentState = root;
+	for (int i = 0; i < line.length(); i++) {
+		char currentChar = line.at(i);
+		if (std::isspace(currentChar)) {
+			// Token is finished
+			if (currentState != root) {
+				if (currentState == nullptr) {
+
+				}
+				Tokentype type = currentState->getType();
+				std::cout << "Found Token, Type: " << type << std::endl;
+				Token token = Token(type);
+				currentState = root;
+			}
+			continue;
+		}
+		currentState = currentState->processChar(currentChar);
+		if (currentState == nullptr) {
+			std::cout << "ERROR in line..."; // TODO
+			break; // TODO: allow multiple errors per line, 
+		}
+	}
 }

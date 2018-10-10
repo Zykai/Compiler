7e207d6698c4ae8baf3d7d110c1f20843950c60b
Tobias Palzer <tobiaspalzer.tp@gmail.com>
2018-10-10 14:54:39 +0200

    add utility functions

--- Compiler/Token.cpp
+++ Compiler/Token.cpp
@@ -2,6 +2,53 @@
 
 #include "Token.h"
 
-Token::Token(Tokentype t) {
+Token::Token(Tokentype t, std::string v) {
 	this->type = t;
+	this->value = v;
+}
+
+std::string Token::getValue() {
+	return this->value;
+}
+
+// TODO: more types
+std::string Token::getTypeString() {
+	switch (this->type) {
+	case identifier:
+		return "Identifier";
+	case integerNumber:
+		return "Integer";
+	case floatNumber:
+		return "Float";
+	case typeName:
+		return "Typename";
+	case importStmt:
+		return "Import";
+	case whileStmt:
+		return "While";
+	case ifToken:
+		return "If";
+	case semicolon:
+		return "Semicolon";
+	case curlyBracesOpen:
+		return "CurlyBraceOpen";
+	case curlyBracesClose:
+		return "CurlyBraceClose";
+	case parentheseOpen:
+		return "ParentheseOpen";
+	case parentheseClose:
+		return "ParentheseClose";
+	case assignOperator:
+		return "AssignmentOperator";
+	default:
+		return "Unknown Type";
+	}
+}
+
+std::string Token::getDescription() {
+	return "<" + this->getTypeString() + ", " + this->getValue() + ">";
+}
+
+Tokentype Token::getType() {
+	return this->type;
 }

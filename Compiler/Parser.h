4832b2acc1cc0cdc67f0d11f963f93153216c539
Tobias Palzer <tobiaspalzer.tp@gmail.com>
2018-11-19 16:19:29 +0100

    add class body

--- Compiler/Parser.h
+++ Compiler/Parser.h
@@ -1,8 +1,16 @@
 #pragma once
-
+#include <list>
+#include "Token.h"
 
 class Parser {
 public:
-
+	Parser();
+	void setTokenList(std::list<std::list<Token>>* list);
+	void startParsing();
 private:
+	Token * getNextToken();
+	std::list<std::list<Token>>::iterator currentLine;
+	std::list<Token>::iterator currentToken;
+	int lineNumber, tokenNumber;
+	std::list<std::list<Token>>* tokenList;
 };

f1b93d4859a4338144eea48658463dad7ff953f3
Tobias Palzer <tobiaspalzer.tp@gmail.com>
2018-11-25 13:31:26 +0100

    rename file

---
+++ Compiler/ComparisonTree.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ExpressionTree.h"
+#include "Token.h"
+
+class ComparisonTree : public ExpressionTree {
+public:
+
+private:
+	ExpressionTree left, right;
+	Token * comparisonOperator;
+};
